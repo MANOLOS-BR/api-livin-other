1381c67ba38d7d4ef2bef5474f39d132b88a81bf
brect <b.limas93@gmail.com>
2020-09-13 13:00:50 -0300

    ajuste rota profile

--- src/main/java/com/manoloscorp/livinother/resources/ProfileResource.java
+++ src/main/java/com/manoloscorp/livinother/resources/ProfileResource.java
@@ -1,6 +1,7 @@
 package com.manoloscorp.livinother.resources;
 
 import com.manoloscorp.livinother.entities.User;
+import com.manoloscorp.livinother.services.UserServiceImpl;
 import com.manoloscorp.livinother.shared.RestConstants;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -10,19 +11,23 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = RestConstants.APPLICATION_API + RestConstants.PROFILE, produces = MediaType.APPLICATION_JSON_VALUE)
 public class ProfileResource {
 
+  private final UserServiceImpl userService;
+
+  public ProfileResource(UserServiceImpl userService) {
+    this.userService = userService;
+  }
+
   @GetMapping("/{id}")
   public ResponseEntity<?> getProfileById(@PathVariable Long id) {
-//    User user = userService.getUserById(id);
-//    return ResponseEntity.ok().body(user);
-    return null;
+    User user = userService.getUserById(id);
+    return ResponseEntity.ok().body(user);
   }
 
 
   @PutMapping(value = "/{id}")
   public ResponseEntity<User> updateProfile(@PathVariable Long id, @RequestBody User user) {
-//    user = userService.updateUser(id, user);
-//    return ResponseEntity.ok().body(user);
-    return null;
+    user = userService.updateUser(id, user);
+    return ResponseEntity.ok().body(user);
   }
 
 }
